957ef3ec3bb1776f390702729dfa04a930683b57
Magdalena Aretz <magdalena.aretz@rewe-digital.com>
2019-08-29 13:44:30 +0200

    reorganized folder structure

--- show_images.py
+++ show_images.py
@@ -3,9 +3,9 @@ from math import floor
 
 # 1 => rgb => 3 bands 
 # 0 => greyscale => 1 kanal
-# -1 => color + alpha (transparency => 4 Kanäle)
+# -1 => color + alpha (transparency => 4 Kanaele)
 #
-img = cv2.imread('wetteronline.jpg', 1)
+img = cv2.imread('images/wetteronline.jpg', 1)
 
 print(type(img))
 print(img.shape)
@@ -22,4 +22,4 @@ cv2.imwrite('wetteronline_resized.jpg', resized_image_keep_proportions)
 # wait for some time before close after button ist closed in ms
 # 2000 => 2 Seunden
 cv2.waitKey(4000)
-cv2.destryAllWindows()
+cv2.destroyAllWindows()
